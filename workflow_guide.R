5b363e5e08f59fd0bd385636438924813446c69a
soumikp <soumikp@umich.edu>
2020-01-23 11:21:49 -0500

    ready to start work

--- workflow_guide.R
+++ workflow_guide.R
@@ -1,23 +1,23 @@
-library(workflowr)
-wflow_git_config(user.name = "soumikp", user.email = "soumikp@umich.edu", overwrite = TRUE)
-wflow_start("heteroplasmy")
-wflow_build() ##
-wflow_publish() ##
-wflow_publish("analysis/*", "Start my new project")
-
-wflow_publish(c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rmd"),
-              "Publish the initial files for myproject")
-wflow_use_github("soumikp")
-wflow_git_push()
-wflow_open("analysis/first-analysis.Rmd")
-wflow_build() ##
-wflow_publish() ##
-wflow_publish(c("data/sample.txt"),
-              "Add dataset")
-
-wflow_publish(c("workflow_guide.R"),
-              "workflow guide R code added")
-wflow_git_push()
+# library(workflowr)
+# wflow_git_config(user.name = "soumikp", user.email = "soumikp@umich.edu", overwrite = TRUE)
+# wflow_start("heteroplasmy")
+# wflow_build() ##
+# wflow_publish() ##
+# wflow_publish("analysis/*", "Start my new project")
+# 
+# wflow_publish(c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rmd"),
+#               "Publish the initial files for myproject")
+# wflow_use_github("soumikp")
+# wflow_git_push()
+# wflow_open("analysis/first-analysis.Rmd")
+# wflow_build() ##
+# wflow_publish() ##
+# wflow_publish(c("data/sample.txt"),
+#               "Add dataset")
+# 
+# wflow_publish(c("workflow_guide.R"),
+#               "workflow guide R code added")
+# wflow_git_push()
 
 wflow_status() 
 wflow_build()
